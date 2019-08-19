26d627d9e1ebb944d15a7c5d68ffdfabf679c9c1
blueted2 <gregory.holder76@gmail.com>
2019-08-19 22:40:00 +0200

    Tested built version of front-end

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const server = require("http").createServer(app);
 const storage = require("node-persist");
 const ioHandler = require("./ioHandler");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "react-app", "build")));
 
 const devicesRoute = require("./api/routes/devices/");
 storage.init();
@@ -15,8 +18,8 @@ server.listen(5000, null, () => {
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/devices", devicesRoute);
-app.get("", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "react-app", "build", "index.html"));
 });
 
 ioHandler.startSocketServer(server);
