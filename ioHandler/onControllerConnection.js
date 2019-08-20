58626345956079d888bac5fecf0274110af3cb40
blueted2 <gregory.holder76@gmail.com>
2019-08-20 23:45:00 +0200

    Reformated button class and added disabled state when device disconnects

--- ioHandler/onControllerConnection.js
+++ ioHandler/onControllerConnection.js
@@ -1,13 +1,22 @@
 const storage = require("../storage");
 
 onControllerConnection = socket => {
-  socket.on("controllerConnection", (deviceId) => {
-    console.log(`The controller for device of id ${deviceId} has connected.`)
-    storage.updateDevice(deviceId, {"connected": true});
+  socket.on("controllerConnection", deviceId => {
+    console.log(`The controller for device of id ${deviceId} has connected.`);
+    socket.broadcast.emit("controllerConnection", {
+      deviceId: deviceId,
+      connected: true
+    });
+    
+    storage.updateDevice(deviceId, { connected: true });
     socket.on("disconnect", () => {
-      console.log(`The controller for device of id ${deviceId} has disconnected.`)
-      storage.updateDevice(deviceId, {"connected": false});
-    })
+      console.log(`The controller for device of id ${deviceId} has disconnected.`);
+      socket.broadcast.emit("controllerDisconnect", {
+        deviceId: deviceId,
+        connected: false
+      });
+      storage.updateDevice(deviceId, { connected: false });
+    });
   });
 };
 
--- react-app/src/components/switch_1.jsx
+++ react-app/src/components/switch_1.jsx
@@ -5,6 +5,16 @@ class Switch_1 extends Component {
     const { device } = this.props;
     const buttonText =
       device.status.charAt(0).toUpperCase() + device.status.slice(1);
+
+    var buttonClass = "full-button ";
+    if (!device.connected) {
+      buttonClass += "disabled ";
+    } else {
+      if (device.status === "on") {
+        buttonClass += "active ";
+      }
+    }
+
     return (
       <div className="device-container">
         <div className="device-name">
@@ -12,13 +22,16 @@ class Switch_1 extends Component {
         </div>
 
         <button
-          onClick={() =>
-            this.props.onStatusChange(
-              device.deviceId,
-              device.status === "on" ? "off" : "on"
-            )
-          }
-          className={"full-button " + (device.status ==="on" ? "active": "")}
+          onClick={() => {
+            if (device.connected) {
+              this.props.onStatusChange({
+                deviceId: device.deviceId,
+                status: device.status === "on" ? "off" : "on"
+              }
+              );
+            }
+          }}
+          className={buttonClass}
         >
           {buttonText}
         </button>
